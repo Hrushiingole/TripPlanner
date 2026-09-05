c33af185bebc55ef6a483a187894c2f0607504db
Hrushikesh Ingole <hrishikeshingole532@gmail.com>
2026-09-05 21:02:15 +0530

    replaced tavily_tool with tavily remote mcp.

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import certifi
 from dotenv import load_dotenv
@@ -24,8 +25,11 @@ from langchain_core.messages import (
     AnyMessage
 )
 from langchain_groq import ChatGroq
-from tools.tavily_tool import tavily_search
+# from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+
+# mcp imports
+from mcp_client_test import tavily_mcp_search
 
 import os
 
@@ -91,7 +95,9 @@ def flight_agent(state: TravelState):
 # =====================
 def hotel_agent(state: TravelState):
     query=f"Best hotels for {state['user_query']}"
-    hotel_results=tavily_search(query)
+    # hotel_results=tavily_search(query)
+
+    hotel_results=asyncio.run(tavily_mcp_search(query))
 
     return {
         "hotel_results":hotel_results,
--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-import certifi
-
-from tools.tavily_tool import tavily_search
-
-from tools.flight_tool import search_flights
-
-# rs=search_flights("Plan a 7 days Nepal trip from Bangladesh")
-# print(rs)
-
-# res = tavily_search("Best travel destinations in Europe")
-# print(res)
+import asyncio
+from mcp_client_test import get_all_tools,tavily_mcp_search
 
 
-from backend import run_travel_agent
+if __name__=="__main__":
+    query="latest news about ai"
+    asyncio.run(tavily_mcp_search(query))
 
-user_input=input("Enter your travel request: ")
-
-response=run_travel_agent(
-    user_input=user_input,
-    thread_id="test_user"
-)
-
-print("\n FINAL RESPONSE:\n")
-print(response["final_answer"])
